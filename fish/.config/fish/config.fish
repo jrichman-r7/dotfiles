388c3167f1264656ebfd8182d1528ee4cf6eacb7
Jordan Richman <j.richman@me.com>
2026-05-16 01:49:23 -0700

    added example abbreviations

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,9 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 starship init fish | source
+
+abbr -a dot 'cd ~/dotfiles'
+abbr -a dotst 'cd ~/dotfiles; and git status'
+abbr -a dotc 'cd ~/dotfiles; and git add -A; and git commit'
+abbr -a stowr 'cd ~/dotfiles; and stow -R'
+
